c4fa82d83872498705cd4d2b3a65846213861d71
minhyeok08 <ssklas08@gmail.com>
2023-08-01 13:53:11 +0900

    Include를 활용하여 게시판, 리스트 출력

---
+++ SpringWebDIIncludedeProject_XML/src/main/java/com/sist/mapper/DataBoardMapper.java
@@ -0,0 +1,65 @@
+package com.sist.mapper;
+import java.util.*;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+
+import com.sist.vo.*;
+
+public interface DataBoardMapper {
+   @Select("SELECT no,subject,name,TO_CHAR(regdate,'YYYY-MM-DD') as dbday,hit,num "
+		  +"FROM (SELECT no,subject,name,regdate,hit,rownum as num "
+		  +"FROM (SELECT /*+ INDEX_DESC(springDataBoard sdb_no_pk)*/no,subject,name,regdate,hit "
+		  +"FROM springDataBoard)) "
+		  +"WHERE num BETWEEN #{start} AND #{end}")
+   public List<DataBoardVO> databoardListData(Map map);
+   @Select("SELECT CEIL(COUNT(*)/10.0) FROM springDataBoard")
+   public int databoardTotalPage();
+   
+   @SelectKey(keyProperty = "no", resultType = int.class , before = true,
+		   statement = "SELECT NVL(MAX(no)+1,1) as no FROM springDataBoard")
+   @Insert("INSERT INTO springDataBoard(no,name,subject,content,pwd,filename,filesize,filecount) "
+		  +"VALUES(#{no},#{name},#{subject},#{content},"
+		  +"#{pwd},#{filename},#{filesize},#{filecount})")
+   public void databoardInsert(DataBoardVO vo);
+   
+   // 상세보기
+   @Update("UPDATE springDataboard SET "
+   		+ "hit=hit+1 "
+   		+ "WHERE no=#{no}")
+   public void hitIncrement(int no);
+   @Select("SELECT no,name,subject,content,hit,TO_CHAR(regdate,'YYYY-MM-DD')as dbday, "
+   		+ "filename,filesize,filecount "
+   		+ "FROM springDataBoard "
+   		+ "WHERE no=#{no}")
+   public DataBoardVO databoardDetailData(int no);
+   // 수정하기
+   // 삭제하기
+   // 찾기
+   @Select("SELECT no,subject,name,TO_CHAR(regdate,'YYYY-MM-DD') as dbday,hit "
+   		+ "FROM springDataBoard "
+   		+ "WHERE ${fs} LIKE '%'||#{ss}||'%'")
+   // $=> 컬럼명 , 테이블명 , #=> 일반 데이터
+   // name,subject,content => ''
+   // WHERE name LIKE '%aaa%'
+   // WHERE 'name' LIKE (X)
+   
+   public List<DataBoardVO> databoardFindData(Map map);
+   // 수정하기
+   // 비밀번호 검색
+   @Select("SELECT pwd FROM springDataBoard "
+   		+ "WHERE no=#{no}")
+   public String databoardGetPassword(int no);
+   // 수정
+   @Update("UPDATE springDataBoard SET "
+   		+ "name=#{name},subject=#{subject},content=#{content} "
+   		+ "WHERE no=#{no}")
+   public void databoardUpdate(DataBoardVO vo);
+   
+   // 삭제
+   @Delete("DELETE FROM springDataBoard WHERE no=#{no}")
+   public void databoardDelete(int no);
+}
---
+++ SpringWebDIIncludedeProject_XML/src/main/java/com/sist/web/FoodController.java
@@ -0,0 +1,61 @@
+package com.sist.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sist.dao.*;
+import com.sist.vo.*;
+@Controller
+public class FoodController {
+	@Autowired
+	private FoodDAO dao;
+	
+	@GetMapping("food/food_list.do")
+	public String food_list(int cno,Model model)
+	{
+		// 데이터베이스에서 데이터 읽기 =? food_list.jsp로 전송 => 화면 출력
+		CategoryVO vo=dao.foodCategoryInfoData(cno);
+		List<FoodVO> list=dao.foodListData(cno);
+		
+		model.addAttribute("cvo", vo);
+		model.addAttribute("list", list);
+		model.addAttribute("main_jsp", "../food/food_list.jsp");
+		return "main/main";	
+	}
+	// food/food_before_detail.do
+	@GetMapping("food/food_before_detail.do")
+	public String food_before_detail(int fno,RedirectAttributes ra,HttpServletResponse response)
+	{
+		/*
+		 * 	model : forward일때 값을 전송
+		 * 	RedirectAttributes : sendRedirect일 때 값을 전송
+		 */
+		Cookie cookie=new Cookie("food_"+fno, String.valueOf(fno));
+		// cookie는 저장이 String만 된다
+		cookie.setPath("/");
+		cookie.setMaxAge(60*60*24);
+		// 브라우저로 전송
+		response.addCookie(cookie);
+		ra.addAttribute("fno", fno);
+		return "redirect:../food/food_detail.do";
+	}
+	// food/food_detail.do?fno=${vo.fno }
+	@GetMapping("food/food_detail.do")
+	public String food_detail(int fno,Model model)
+	{
+		// 상세보기에 필요한 데이터를 오라클에서 읽어 온다
+		// 전송
+		FoodVO vo=dao.foodDetailData(fno);
+		model.addAttribute("vo", vo);
+		model.addAttribute("main_jsp", "../food/food_detail.jsp");
+		return "main/main";
+	}
+}
