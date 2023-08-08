28d4e0cf5374c37610448e6a70dd99f106f38689
minhyeok08 <ssklas08@gmail.com>
2023-08-08 17:57:20 +0900

    vue를 이용한 리스트 출력, 검색 기능 연습

---
+++ SpringFrontProject_2/src/main/java/com/sist/web/FoodController.java
@@ -0,0 +1,27 @@
+package com.sist.web;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sist.dao.*;
+import com.sist.vo.*;
+@Controller
+public class FoodController {
+	@Autowired
+	private FoodDAO dao;
+	
+	@GetMapping("food/list.do")	
+	public String food_list()
+	{
+		return "food/list";
+	}
+	@GetMapping("food/find.do")
+	public String food_find()
+	{
+		return "food/find";
+	}
+}
+
